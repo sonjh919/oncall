b62835e02bf165b47aeadb784b60ede7397ff154
sonjh919 <kingjh1125@gmail.com>
2024-12-10 20:21:35 +0900

    docs: Create function list

--- src/main/java/oncall/controller/OncallController.java
+++ src/main/java/oncall/controller/OncallController.java
@@ -10,9 +10,10 @@ public class OncallController {
     }
 
     public void start(){
-        // 플로우. 리드미의 구현 기능 목록 따라가기
-        //입력
-
+//        createStartDayFromUserInput();
+//        createOncallFromUserInput();
+//        changeOncallSchedule();
+//        outputOncall();
     }
 
 }
